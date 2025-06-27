aa8319d2a496a1dc60b67209c135b36b408b272d
deoncg88 <139814745+deoncg88@users.noreply.github.com>
2025-06-27 05:55:59 +0000

    Add Teams page

---
+++ src/pages/Teams.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TeamList from "../components/TeamList";
+
+const Teams = () => {
+    return (
+        <div style={{ padding: "20px" }}>
+            <h1>AI Teams</h1>
+            <TeamList />
+        </div>
+    );
+};
+
+export default Teams;
