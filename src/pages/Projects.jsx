659b5ef9b941b7d20f755f1b0ef4e834398270fa
deoncg88 <139814745+deoncg88@users.noreply.github.com>
2025-06-27 05:53:42 +0000

    Add Projects page

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProjectList from "../components/ProjectList";
+
+const Projects = () => {
+    return (
+        <div style={{ padding: "20px" }}>
+            <h1>All Projects</h1>
+            <ProjectList />
+        </div>
+    );
+};
+
+export default Projects;
