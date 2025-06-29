f4f82f5e8253d6142ea1ec224a5c7ceb96016922
deoncg88 <139814745+deoncg88@users.noreply.github.com>
2025-06-29 16:08:02 +0000

    src/Main.jsx update

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import TeamDashboard from './pages/TeamDashboard';  // or your main App component
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TeamDashboard />  {/* or <App /> if you have an App component */}
+  </React.StrictMode>
+);
